3ebb45ba75775ef614a193acd7f37971793171b2
Reece Emero <reece.emero98@gmail.com>
2019-04-22 05:18:49 -0400

    Added index route

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const path = require("path")
 
+// Routers
+const indexRouter = require("./routes/index")
+
 const app = express()
 
 // View engine
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 
-// Body parser middleware
+// Middleware
 app.use(bodyParser.json()) // Extract data from request body
 app.use(bodyParser.urlencoded({ extended: false })) // Extract data from url
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+// Set public folder
+app.use(express.static(path.join(__dirname, "public")))
+
+// Set routes
+app.use("/", indexRouter)
 
 const port = 3000
 
