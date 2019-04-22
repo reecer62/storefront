e63d770f2c3197c42481b626f0fffabc9f07470f
Reece Emero <reece.emero98@gmail.com>
2019-04-22 04:02:45 -0400

    Created server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("node started")
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+const port = 3000
+
+// Start server
+app.listen(port, () => console.log(`Server started on port ${port}`))
