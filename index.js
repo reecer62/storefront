d7c5372696e3ca86828b94d8abd3d352c54f6e08
Reece Emero <reece.emero98@gmail.com>
2019-04-22 04:18:17 -0400

    Added body parser middleware

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
 const app = express()
+
+// Body parser middleware
+app.use(bodyParser.json()) // Extract data from request body
+app.use(bodyParser.urlencoded({ extended: false })) // Extract data from url
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
