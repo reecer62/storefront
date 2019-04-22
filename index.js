5fbe7a3c6c2e81fe4888467307895c69359714d9
Reece Emero <reece.emero98@gmail.com>
2019-04-22 05:09:39 -0400

    Added pug templating engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path = require("path")
 
 const app = express()
 
+// View engine
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
 // Body parser middleware
 app.use(bodyParser.json()) // Extract data from request body
 app.use(bodyParser.urlencoded({ extended: false })) // Extract data from url
